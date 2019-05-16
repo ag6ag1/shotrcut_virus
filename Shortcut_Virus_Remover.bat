411b9a3a9f6bd78421aeabd6eb912e5472c9a0a9
ag6ag1 <ag6ag@live.com>
2019-05-17 01:01:58 +0300

    Create Shortcut_Virus_Remover.bat

---
+++ Shortcut_Virus_Remover.bat
@@ -0,0 +1,77 @@
+echo off
+color 9f
+cls
+echo.
+echo.
+echo.
+echo.
+echo  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo  ++                                                                          ++
+echo  ++                                                                          ++
+echo  ++               ***      ShortCut Virus Remover by ag6ag      ***          ++
+echo  ++                                                                          ++
+echo  ++                                                                          ++
+echo  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo.
+echo.
+echo  Recommend only to use in Flash drives.
+echo.
+echo  If this file is on your Desktop it will badly affect to your Desktop Icons !!!
+echo.
+echo.
+echo.
+echo.
+echo  Shortcut Virus Remover is going to repair your Flash drive..........
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo  Cleaning process will take few seconds..........
+echo  Please wait..........
+echo.
+echo.
+echo.
+del *.lnk
+del fypuas.exe
+del fypuasx.exe
+cls
+echo.
+echo.
+echo  Removing Shortcuts...........     Complete.
+echo.
+echo.
+echo.
+echo  Make visible your folder...........     is in process..........
+echo  This process will take few minutes..........
+echo.
+echo  Please wait..........
+attrib /s /d -s -h -r *.*
+cls
+echo.
+echo.
+echo.
+echo  Shortcut Virus Remover finish its Job.........
+echo.
+echo  Your Flash drive is OK now
+echo.
+echo.
+echo.
+echo  Check your Flash drive for unknown files and folders.
+echo  If there is any, delete them.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo  Cleaning Process Complete.
+echo.
+echo.
+echo  Developed by Eranda.
+echo.
+echo.
+echo.
+pause
